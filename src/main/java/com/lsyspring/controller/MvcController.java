813900b65e40fb1aa151adc24c38cd2ab29db1e8
Haughlizard <lishengyan1991@qq.com>
2017-09-20 22:14:09 +0800

    第一次提交

---
+++ src/main/java/com/lsyspring/controller/MvcController.java
@@ -0,0 +1,13 @@
+package com.lsyspring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mvc")
+public class MvcController {
+    @RequestMapping("/Hello")
+    public String hello(){
+        return "hello";
+    }
+}
